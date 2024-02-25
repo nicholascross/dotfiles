56e55c29ab668211a060a14801bb560778c9c060
Nicholas Cross <nicholascross@192-168-1-102.tpgi.com.au>
2024-02-26 08:58:23 +1100

    Add right prompt with last command exit status and duration

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -27,3 +27,31 @@ add-zsh-hook chpwd set_prompt
 
 zle -N minimise_prompt
 bindkey "^M" minimise_prompt
+
+cmd_start_time=0
+
+preexec_rprompt() {
+    cmd_start_time=$(date +%s)
+}
+
+precmd_rprompt() {
+    cmd_exit_status=$?
+    if [[ $cmd_start_time -ne 0 ]]; then
+        cmd_end_time=$(date +%s)
+        cmd_duration=$((cmd_end_time - cmd_start_time))
+
+        if [[ $cmd_exit_status -eq 0 ]]; then
+            status_icon="%F{34}%f%K{34}%F{22}✔ %f%k"
+            RPROMPT="${status_icon}%K{34}%F{22}  ${cmd_duration}s %f%k"
+        else
+            status_icon="%F{214}%f%K{214}%F{red}✖ $cmd_exit_status %f%k"
+            RPROMPT="${status_icon}%K{214}%F{red}  ${cmd_duration}s %f%k"
+        fi
+    else
+        RPROMPT=""
+    fi
+}
+
+add-zsh-hook preexec preexec_rprompt
+add-zsh-hook precmd precmd_rprompt
+
