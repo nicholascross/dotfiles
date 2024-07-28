7b43c7b7edc8901743e0ff457b7258143cc3d119
Nicholas Cross <nicholas@nacrossweb.com>
2024-07-28 14:37:04 +1000

    Add option to set secret if not set
    Remove assign to env since this was export the secret and it is better to just keep it locally where ever it is used

--- .zsh/secret_storage.zsh
+++ .zsh/secret_storage.zsh
@@ -18,16 +18,16 @@ function retrieve_secret() {
   security find-generic-password -a "${account_name}" -s "${service_name}" -w
 }
 
-# assign_secret_to_env "MY_SECRET_ENV" "MyService" "myAccount"
-function assign_secret_to_env() {
-  local env_var_name="$1"
-  local service_name="$2"
-  local account_name="$3"
+function enforce_secret() {
+  local service_name="$1"
+  local account_name="$2"
 
-  # Use retrieve_secret function to get the secret
-  local secret_value=$(retrieve_secret "$service_name" "$account_name")
+  # Check if the secret already exists
+  if retrieve_secret "${service_name}" "${account_name}" > /dev/null 2>&1; then
+    return 0
+  fi
 
-  # Assign the retrieved secret to the specified environment variable
-  eval export "$env_var_name"="'$secret_value'"
+  # If the secret doesn't exist, store a new one
+  store_secret "${service_name}" "${account_name}"
 }
 
