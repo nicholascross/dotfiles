7ccb7ed3f93c8733cf1c0e47c0cdd02384f630ab
Nicholas Cross <nicholas@nacrossweb.com>
2024-08-17 19:43:11 +1000

    Add standard indentation using tab

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,12 +2,20 @@ local wk = require("which-key")
 local builtin = require('telescope.builtin')
 
 wk.add({
+  -- File browsing
   { "<leader>f", group = "Telescope" },
   { "<leader>fb", builtin.buffers, desc = "Buffers" },
   { "<leader>ff", builtin.find_files, desc = "Find Files" },
   { "<leader>fg", builtin.live_grep, desc = "Live Grep" },
   { "<leader>fh", builtin.help_tags, desc = "Help Tags" },
 
-  { "<space>-", require("oil").toggle_float, desc = "Open file browser" }
+  -- File management
+  { "<space>-", require("oil").toggle_float, desc = "Open file browser" },
+
+  -- Indentation
+  { "<Tab>", ">>", mode = { "n" } },
+  { "<S-Tab>", "<<", mode = { "n" } },
+  { "<Tab>", ">gv", mode = { "v" } },
+  { "<S-Tab>", "<gv", mode = { "v" } },
 })
 
