45a8c952e814efb05f3388983c126bdf9d5a443d
Nicholas Cross <nicholas@nacrossweb.com>
2024-09-01 18:22:28 +1000

    Fix which key health check issues

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -58,7 +58,16 @@ wk.add({
   { "<leader>bl", "<Cmd>BufferOrderByLanguage<CR>", desc = "Sort by language" },
   { "<leader>bw", "<Cmd>BufferOrderByWindowNumber<CR>", desc = "Sort by window number" },
 
-  { "<leader>y", ":lua CopyToClipboard()<CR>", desc = "Yank to system clipboard", mode = { "v" } }
+  { "<leader>y", ":lua CopyToClipboard()<CR>", desc = "Yank to system clipboard", mode = { "v" } },
 
+  { "K", vim.lsp.buf.hover, desc = "LSP hover info" },
+  { "[g", vim.diagnostic.goto_prev, desc = "Go to previous diagnostic" },
+  { "gD", vim.lsp.buf.declaration, desc = "LSP go to declaration" },
+  { "g]", vim.diagnostic.goto_next, desc = "Go to next diagnostic" },
+  { "gd", vim.lsp.buf.definition, desc = "LSP go to definition" },
+  { "gi", vim.lsp.buf.implementation, desc = "LSP go to implementation" },
+  { "gn", vim.lsp.buf.rename, desc = "LSP rename" },
+  { "gr", vim.lsp.buf.references, desc = "LSP list references" },
+  { "gs", vim.lsp.buf.signature_help, desc = "LSP signature help" },
 })
 
