b44acd8e6e685151180597891b23cb93884af897
Nicholas Cross <nicholas@nacrossweb.com>
2024-09-08 18:20:28 +1000

    Change gutter icons used for debugging

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -46,5 +46,10 @@ return {
         args = {},
       },
     }
+
+    vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'DiagnosticSignWarn', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DiagnosticSignInfo', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapStopped', { text = '', texthl = 'DiagnosticSignInfo', linehl = 'Visual', numhl = 'DiagnosticSignInfo' })
   end
 }
