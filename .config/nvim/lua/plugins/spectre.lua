0f703ebd871d290cc13508675931d1ee84bc17a7
Nicholas Cross <nicholas@nacrossweb.com>
2024-09-16 19:56:45 +1000

    Fix compatability issues in spectre

--- .config/nvim/lua/plugins/spectre.lua
+++ .config/nvim/lua/plugins/spectre.lua
@@ -1,1 +1,24 @@
-return { "nvim-pack/nvim-spectre" }
+return {
+  "nvim-pack/nvim-spectre",
+  config = function()
+    require('spectre').setup({
+      mapping={
+        ['send_to_qf'] = {
+          map = "<leader>x",
+          cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+          desc = "send all items to quickfix"
+        },
+      },
+      replace_engine = {
+        ["sed"] = {
+          cmd = "sed",
+          args = {
+            "-i",
+            "",
+            "-E",
+          },
+        },
+      }
+    })
+  end
+}
