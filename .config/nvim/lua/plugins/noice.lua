50fa12d2241e1b591409662c458bc501740739e7
Nicholas Cross <nicholas@nacrossweb.com>
2024-09-29 12:56:34 +1000

    Add noice and dressing plugins
    Noice for command line and messaging enhancement
    Dressing was also added primarily as a work around for noice.nvim issue
    #938 which is causing vim.ui.select to be blocked from presenting
    prompts for the selection until after the selection is made.  Incidently
    dressing also enhances the selection experience.

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,33 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    select = {
+      enabled = true,      -- Enable the select UI
+      backend = "telescope",     -- Options: 'nui', 'builtin', 'telescope', 'fzf_lua', etc.
+      nui = {
+        position = "50%",
+        size = nil,
+        relative = "editor",
+        border = {
+          style = "rounded",
+        },
+        buf_options = {
+          swapfile = false,
+          filetype = "NoiceSelect",
+        },
+        win_options = {
+          winblend = 10,
+        },
+      },
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+}
