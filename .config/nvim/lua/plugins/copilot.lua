8c79f0162e67c49da9db1bedad1ddf553270af45
Nicholas Cross <nicholas@nacrossweb.com>
2024-10-27 09:45:14 +1100

    Add copilot.lua plugin
    By default this plugin is disabled unless copilot is already
    authenticated on the system.
    
    Additionally can be hardcoded to off using enabled local variable.

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cond = function()
+    local enabled = true
+    local home = vim.fn.expand("$HOME")
+    -- Path to the Copilot authentication file
+    local auth_file = home .. "/.config/github-copilot/hosts.json"
+
+    -- Check if the file exists and is readable
+    return enabled and vim.fn.filereadable(auth_file) == 1
+  end,
+  config = function()
+    require("copilot").setup({})
+  end,
+}
