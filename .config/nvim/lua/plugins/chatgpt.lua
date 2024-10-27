9985a67592680f35947dc14826d9b91c2876a053
Nicholas Cross <nicholas@nacrossweb.com>
2024-10-27 11:55:52 +1100

    Update chatgpt config

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -15,7 +15,16 @@ return {
     require("chatgpt").setup({
       -- Use the macOS keychain to retrieve the API key
       api_key_cmd = "security find-generic-password -s OPENAI_APIKEY -w",
-      -- Add any other configuration options here
+
+      openai_params = {
+        model = "gpt-4o-2024-08-06",
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        max_tokens = 4095,
+        temperature = 0.2,
+        top_p = 0.1,
+        n = 1,
+      }
     })
   end,
   dependencies = {
