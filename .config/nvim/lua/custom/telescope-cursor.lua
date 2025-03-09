04fc8a3aff483f06906fe9dfde9bc9f251908761
Nicholas Cross <nicholas@nacrossweb.com>
2025-03-09 17:34:57 +1100

    Rename custom module and add cursor-based file search
    • Renamed "live-grep-args-as-type.lua" to "telescope-cursor.lua" and updated the internal function name from grep_word_under_cursor_same_type to grep_cursor.
    • Updated keymaps to use the new module/function naming.
    • Added a new "find_cursor" function and keymap (<leader>fn) for finding files that include the word under the cursor.

--- .config/nvim/lua/custom/telescope-cursor.lua
+++ .config/nvim/lua/custom/telescope-cursor.lua
@@ -2,7 +2,7 @@ local live_grep_args = require("telescope").extensions.live_grep_args.live_grep_
 
 local M = {}
 
-M.grep_word_under_cursor_same_type = function()
+M.grep_cursor = function()
   local word_under_cursor = vim.fn.expand("<cword>")
   local extension = vim.fn.expand('%:e')
   local default_text = '"' .. word_under_cursor .. '"'
@@ -12,6 +12,10 @@ M.grep_word_under_cursor_same_type = function()
   end
 
   live_grep_args({ default_text = default_text })
+end
+
+M.find_cursor = function()
+  require('telescope.builtin').find_files { find_command = { "fd", "--type", "f", "--glob", "*" .. vim.fn.expand("<cword>") .. "*" } }
 end
 
 return M
